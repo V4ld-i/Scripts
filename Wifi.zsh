1bd80e9926a1bdbb590018ed53e579657886b7ed
V4ld-i <techv4ld1@gmail.com>
2024-07-13 22:27:44 +0200

    Wifi Creado

--- Wifi.zsh
+++ Wifi.zsh
@@ -1,7 +1,6 @@
-#!bin/zsh/
-# Script que al pasar como argumento una url te descarga la imagen de esta
-# -------------------------------------------------
+#!/bin/zsh
 #
+#Script que muestra las conexiones wifis disponibles.
 #  ██▒   █▓▄▄▄      ██▓   ▓█████▄ ██▓
 # ▓██░   █▒████▄   ▓██▒   ▒██▀ ██▓██▒
 #  ▓██  █▒▒██  ▀█▄ ▒██░   ░██   █▒██▒
@@ -14,18 +13,4 @@
 #      ░                   ░
 # -------------------------------------------------
 
-# Verificamos si se proporciono un argumento
-if [[ $# -lt 2 ]]; then
-    echo -e "Es \e[31mnecesario\e[0m que: $0 <URL de la imagen> <nombre de la imágen>"
-    exit 1
-fi
 
-# Obtenemos la URL de la imagen
-url=$1
-
-# Obtenemos el nombre de la imagen
-nombre=$2
-
-curl -o "$nombre" "$url"
-
-echo -e "Imagen descargada \e[32mcorrectamente\e[0m"
